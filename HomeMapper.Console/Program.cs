7f465246b8c7ffb30c07999d5a5b0bcb7adbe596
John Muraski <john.t.muraski@outlook.com>
2026-06-15 07:42:26 -0500

    Initial

---
+++ HomeMapper.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using SharpPcap;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("[bold green]HomeMapper Console Application[/]");
+AnsiConsole.MarkupLine("Searching for devices....");
+
+var devices = CaptureDeviceList.Instance;
+
+foreach(var device in devices)
+{
+    AnsiConsole.MarkupLine($"[blue]{device.Name}[/] - [green]{device.Description}[/]");
+}
+
+AnsiConsole.MarkupLine("");
+AnsiConsole.Markup("[green]Scan complete.[/]");
+Console.ReadLine();
