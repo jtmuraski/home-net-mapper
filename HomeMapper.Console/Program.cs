f2da1bc9d3199bdd1ff3825c3ea422813049f1e3
John Muraski <john.t.muraski@outlook.com>
2026-06-28 07:16:23 -0500

    Update

--- HomeMapper.Console/Program.cs
+++ HomeMapper.Console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using SharpPcap;
 using Spectre.Console;
+using System.Net;
+using System.Net.NetworkInformation;
 
 AnsiConsole.MarkupLine("[bold green]HomeMapper Console Application[/]");
 AnsiConsole.MarkupLine("Searching for devices....");
@@ -14,5 +16,26 @@ foreach(var device in devices)
 AnsiConsole.MarkupLine("");
 AnsiConsole.Markup("[green]Scan complete.[/]");
 Console.ReadLine();
+
+AnsiConsole.MarkupLine("Scanning Network addresses now");
+
+var networkDevices = NetworkInterface.GetAllNetworkInterfaces();
+foreach(var device in networkDevices)
+{
+   if(device.OperationalStatus == OperationalStatus.Up && 
+        device.NetworkInterfaceType != NetworkInterfaceType.Loopback && 
+        (device.NetworkInterfaceType == NetworkInterfaceType.Ethernet || device.NetworkInterfaceType == NetworkInterfaceType.Wireless80211))
+    {
+        foreach(var address in device.GetIPProperties().UnicastAddresses)
+        {
+            if(address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                var network = IPNetwork2.Parse(address.Address, address.IPv4Mask);
+                var deviceAddresses = network.ListIPAddress(Filter.Usable);
+                AnsiConsole.MarkupLine($"[blue]{device.Name}[/] - [green]{address.Address}[/] - [yellow]{address.IPv4Mask}[/]");
+            }
+        }
+    }
+}
 
 // Making a change to force git to track it
